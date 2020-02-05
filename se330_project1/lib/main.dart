49193839097164497f778660710196b321d9b5e5
Gina.Guidotti <gina.guidotti@jacks.sdstate.edu>
2020-02-05 10:47:22 -0600

    Try #3 Please work - Branch Gina

--- se330_project1/lib/main.dart
+++ se330_project1/lib/main.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Just Clicked Demo',
+      title: 'Just Clicked Demo', 
       theme: ThemeData( 
         primarySwatch: Colors.blue,
       ),
