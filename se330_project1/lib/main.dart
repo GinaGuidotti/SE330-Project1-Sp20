4bb963eab8f68357f18e203f2360ce830ebef8f9
Gina.Guidotti <gina.guidotti@jacks.sdstate.edu>
2020-02-05 10:45:50 -0600

    Lets try this again...Branch Gina?

--- se330_project1/lib/main.dart
+++ se330_project1/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData( 
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: '*JustClicked* Demo Home Page'),
+      home: MyHomePage(title: 'JustClicked Demo Home Page'),
     );
   }
 }
