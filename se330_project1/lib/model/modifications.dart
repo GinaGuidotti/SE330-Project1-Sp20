65f005fe7e8a7f3bbd44beb8dd49c22586f3f337
Gina Guidotti <Gina.Guidotti@jacks.sdstate.edu>
2020-03-12 00:30:36 -0500

    Shop Page and Individual Items Page are done, Working on modifications screen, will finish tomorrow

--- se330_project1/lib/model/modifications.dart
+++ se330_project1/lib/model/modifications.dart
@@ -1,10 +1,14 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
 
 enum ColorChoice{
   black,    //0
 }
+
 enum LensStyle{
-  none,           //0
+  no_Extra,           //0
   lens18_55mm,    //1
   lens18_140mm,   //2
   lens70_300mm,   //3
@@ -14,7 +18,7 @@ enum ConfigPack{
   basePlusAdobeCreativeCloud, //1
 }
 enum ProtectionPlan{
-  none,                 //0
+  no_Plan,                 //0
   twoYearsProtection,   //1
   threeYearsProtection, //2
 }
@@ -25,14 +29,197 @@ enum ExtraMemoryPack{
 }
 
 
+List<String> colorChoiceString = ["black"];    
+
+List<String> lensStyleString = [
+  "No Extra Lens",           //0
+  "18-55mm",    //1
+  "18-140mm",   //2
+  "70-300mm",   //3
+];
+
+List<String> configPackString = [
+  "Base Pack",                   //0
+  "Base + Adobe Creative Cloud", //1
+];
+
+List<String> protectionPlanString = [
+  "No Extra Plan",                 //0
+  "Two Year Plan",   //1
+  "Three Year Plan", //2
+];
+
+List<String> extraMemoryPackString = [
+  "No Extra Memory",             //0
+  "SanDisk 32GB",  //1
+  "SanDisk 64GB",  //2
+];
+
 
 class ModificationsDatabase {
   ColorChoice theColorChoice = ColorChoice.black;
-  LensStyle theLensChoice = LensStyle.none;  
+  LensStyle theLensChoice = LensStyle.no_Extra;  
   ConfigPack theConfigPack = ConfigPack.basePack;
-  ProtectionPlan theProtectionPlan = ProtectionPlan.none;
+  ProtectionPlan theProtectionPlan = ProtectionPlan.no_Plan;
   ExtraMemoryPack theMemoryChoice = ExtraMemoryPack.basePack;
 
-  ModificationsDatabase({this.theColorChoice, this.theLensChoice, this.theConfigPack, this.theProtectionPlan, this.theMemoryChoice});
+  ModificationsDatabase();
+  void setAllData(int colorChoice, int lensChoice, int confChoice, int protChoice, int memoryChoice){
+    if(colorChoice == 0){
+      theColorChoice = ColorChoice.black;
+    }else{
+      theColorChoice = ColorChoice.black;      
+    }   
+    
+    if(lensChoice == 1){
+      theLensChoice = LensStyle.lens18_55mm;
+    }else if(lensChoice == 2){
+      theLensChoice = LensStyle.lens18_140mm;
+    }else if(lensChoice == 3){
+      theLensChoice = LensStyle.lens70_300mm;
+    }else{
+      theLensChoice = LensStyle.no_Extra;
+    }
  
+    if(confChoice == 1){
+      theConfigPack = ConfigPack.basePack;
+    }else{
+      theConfigPack = ConfigPack.basePlusAdobeCreativeCloud;
+    }
+
+    if(protChoice == 1){
+      theProtectionPlan = ProtectionPlan.twoYearsProtection;
+    }else if(protChoice == 2){
+      theProtectionPlan = ProtectionPlan.threeYearsProtection; 
+    }else{
+      theProtectionPlan = ProtectionPlan.no_Plan;
+    }
+
+    if(memoryChoice == 1){
+      theMemoryChoice = ExtraMemoryPack.basePlusSanDisk32GB;
+    }else if(memoryChoice == 2){
+      theMemoryChoice = ExtraMemoryPack.basePlusSanDisk64GB; 
+    }else{
+      theMemoryChoice = ExtraMemoryPack.basePack;
+    }
+
+  }
+  double getColorPrice(int enumVal){
+    double price = 0;
+    if(enumVal == 0){
+      price = 0.00; //Black
+    }
+    return price;
+  }
+  double getExtraLensPrice(int enumVal){
+    double price = 0;
+    if(enumVal == 1){
+      price = 59.99; //18-55mm
+    }else if(enumVal == 2){
+      price = 50.95; //18-140mm
+    }else if(enumVal == 3){
+      price = 51.86; //70-300mm
+    }else{
+      price = 0; //None
+    }
+    return price;
+  }
+  double getConfigPackPrice(int enumVal){
+    double price = 0;
+    if(enumVal == 1){
+      price = 118.88; //Base+Adobe
+    }else{
+      price = 0; //Base Pack
+    }
+    return price;
+  }
+  double getProtectPlanPrice(int enumVal){
+    double price = 0;
+    if(enumVal == 1){
+      price = 24.99; //2 Years
+    }else if(enumVal == 2){
+      price = 33.99; //3 Years
+    }else{
+      price = 0;
+    }
+    return price;
+  }
+  double getExtraMemoryPrice(int enumVal){
+    double price = 0;
+    if(enumVal == 1){
+      price = 5.75; //Base+32GB
+    }else if(enumVal == 2){
+      price = 11.49; //Base+64GB
+    }else{
+      price = 0.00; 
+    }
+    return price;
+  }
+
+  int returnNumberColor(){
+    int returnNum = 0;
+    if(theColorChoice == ColorChoice.black){
+      returnNum = 0;
+    }
+    return returnNum;
+  }
+
+  int returnNumberLens(){
+    int returnNum = 0;
+    if(theLensChoice == LensStyle.lens18_55mm){
+      returnNum = 1;
+    }else if(theLensChoice == LensStyle.lens18_140mm){
+      returnNum = 2;
+    }else if(theLensChoice == LensStyle.lens70_300mm){
+      returnNum = 3;
+    }else{
+      returnNum = 0;
+    }
+    return returnNum;
+  }
+
+  int returnNumberConfig(){
+    int returnNum = 0;
+    if(theConfigPack == ConfigPack.basePlusAdobeCreativeCloud){
+      returnNum = 1;
+    }else{
+      returnNum = 0;
+    }
+    return returnNum;
+  }
+
+  int returnNumberProtectionPlan(){
+    int returnNum = 0;
+    if(theProtectionPlan == ProtectionPlan.twoYearsProtection){
+      returnNum = 1;
+    }else if(theProtectionPlan == ProtectionPlan.threeYearsProtection){
+      returnNum = 2;
+    }else{
+      returnNum = 0;
+    }
+    return returnNum;
+  }
+
+  int returnNumberExtraCard(){
+    int returnNum = 0;
+    if(theMemoryChoice == ExtraMemoryPack.basePlusSanDisk32GB){
+      returnNum = 1;
+    }else if(theMemoryChoice == ExtraMemoryPack.basePlusSanDisk64GB){
+      returnNum = 2;
+    }else{
+      returnNum = 0;
+    }
+    return returnNum;
+  }
+
+  bool checkStringColor(String theName){
+    bool foundVal = false;
+    for(int i = 0; i < colorChoiceString.length; i++){
+      if(colorChoiceString[i] == theName){
+        foundVal = true;
+      }
+    }
+    return foundVal; 
+  }
+
 } 
--- se330_project1/lib/screens/home.dart
+++ se330_project1/lib/screens/home.dart
@@ -1,193 +1,76 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';  
 import 'package:se330_project1/globalvariables.dart';
-import 'package:se330_project1/model/categorycontainer.dart';
-import 'package:se330_project1/model/productcontainer.dart';
+import 'package:se330_project1/navigation/custom_navigation_drawer.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomePage extends StatefulWidget { 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xfff9f9f9),
-      bottomNavigationBar: BottomNavigationBar(
-        unselectedItemColor: Colors.grey,
-        selectedItemColor: Colors.blueAccent,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.title),
-            title: Text("Home"),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.card_giftcard),
-            title: Text("Gift"),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite_border),
-            title: Text("Favorite"),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_outline),
-            title: Text("Person"),
-          ),
-        ],
-      ),
-      appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.black54),
-        backgroundColor: Color(0xfff9f9f9),
-        elevation: 0.0,
-        leading: IconButton(
-          icon: Icon(
-            Icons.menu,
-          ),
-          onPressed: () {},
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.shopping_basket),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              "Food",
-              style: Theme.of(context).textTheme.display1.apply(
-                    fontWeightDelta: 2,
-                    color: Colors.black,
-                  ),
-            ),
-            Text("Delivery",
-                style:
-                    Theme.of(context).textTheme.display1.copyWith(height: .9)),
-            SizedBox(
-              height: 15,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                fillColor: Colors.white,
-                filled: true,
-                border: InputBorder.none,
-                prefixIcon: Icon(Icons.search),
-                hintText: "Search",
-              ),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Container(
-              height: 81,
-              child: CategoryContainer(),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Text(
-              "Hamburger",
-              style: Theme.of(context).textTheme.title.apply(
-                    fontWeightDelta: 2,
-                  ),
-            ),
-            SizedBox(
-              height: 11,
-            ),
-            GridView.builder(
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 15,
-                mainAxisSpacing: 15,
-                childAspectRatio: .7
-              ),
-              itemCount: productsList.length,
-              itemBuilder: (BuildContext context, int index) {
-                return ProductContainer(id: index);
-              },
-            )
-          ],
-        ),
-      ),
-    );
-  }
+  createState() => _HomePageState();
 }
 
-// import 'package:flutter/material.dart';  
-// import 'package:se330_project1/globalvariables.dart';
-// import 'package:se330_project1/navigation/custom_navigation_drawer.dart';
+class _HomePageState extends State<HomePage>{ 
 
-// class HomePage extends StatefulWidget { 
-//   @override
-//   createState() => _HomePageState();
-// }
+  Widget build(BuildContext context) {    
+   double screenWidth = MediaQuery.of(context).size.width;
+   double screenHeight = MediaQuery.of(context).size.height; 
+   TextStyle style = TextStyle(fontSize: screenWidth*0.05, color: Colors.black);  
+   TextStyle blueStyle = TextStyle(fontSize: screenWidth*0.05, color: DarkCyan);   
+   TextStyle blueLargeStyle = TextStyle(fontSize: screenWidth*0.07, color: DarkCyan, fontWeight: FontWeight.bold); 
+   TextStyle italicTitle = TextStyle(fontSize: screenWidth*0.065, fontStyle: FontStyle.italic);
+   TextStyle italicBlueTitle = TextStyle(fontSize: screenWidth*0.065, fontStyle: FontStyle.italic, color: DarkCyan, fontWeight: FontWeight.bold);
+   return new Scaffold(  
+        appBar: AppBar(
+          title: Text('Home Page'),
+        ),
+        drawer: CollapsingNavigationDrawer(),
+        body: new Center(
+          child: new ListView( 
+            children: <Widget>[ 
+              SizedBox(height: screenWidth * 0.06),
+              Text(
+                'Just Clicked Cameras',
+                textAlign: TextAlign.center,
+                style: blueLargeStyle,
+              ),
+              SizedBox(height: screenWidth * 0.1),
+              ListTile( 
+                subtitle: Text((
+                  'Tired of scrolling on Instagram or Pinterest wishing you could be on that perfect family vacation '
+                  +'or could take that beautiful of a picture without using a fancy filter?\n'), 
+                  style: blueStyle),
+              ), 
+              ListTile( 
+                subtitle: Text(( 
+                  'Well now you can!\n'
+                  ),
+                  style: italicBlueTitle),
+              ), 
+              ListTile( 
+                subtitle: Text((  
+                  'Just come check out any of our Just Clicked Camera styles and we can make your wish come true! '),
+                  style: blueStyle),
+              ),
+              SizedBox(height: screenWidth * 0.1),
+              ListTile(
+                title: Text('About Our Company:', style: italicBlueTitle,),
+                subtitle: Text(
+                  'Our three creating developers met during their Undergraduate years at South Dakota State University during '
+                +'the Fall of 2016. Over the years, they had many classes and projects together but in their fourth year of '
+                +'Undergrad they came together and thought of an idea for some product they could sell. '
+                +'They thought and thought for so long until the title just clicked and thus "Just Clicked Cameras" came to be.',
+                  style: blueStyle),
+              ),
+              SizedBox(height: screenWidth * 0.05),
+              ListTile(
+                title: Text('Special Note For Our Customers!', style: italicBlueTitle,),
+                subtitle: Text( 'This is only a project for a class and this does not exist in real life as a shop or a source of professional cameras, '
+                +'so please don\'t think this is a real company and actually use your real payment information. \nThis is just made for fun! (and class too)',
+                  style: blueStyle),
+              ), 
+              SizedBox(height: screenWidth * 0.05),
+            ],
+          ), 
+        ),
+      );
+  }
 
-// class _HomePageState extends State<HomePage>{ 
-
-//   Widget build(BuildContext context) {    
-//    double screenWidth = MediaQuery.of(context).size.width;
-//    double screenHeight = MediaQuery.of(context).size.height; 
-//    TextStyle style = TextStyle(fontSize: screenWidth*0.05, color: Colors.black);  
-//    TextStyle blueStyle = TextStyle(fontSize: screenWidth*0.05, color: DarkCyan);   
-//    TextStyle blueLargeStyle = TextStyle(fontSize: screenWidth*0.07, color: DarkCyan, fontWeight: FontWeight.bold); 
-//    TextStyle italicTitle = TextStyle(fontSize: screenWidth*0.065, fontStyle: FontStyle.italic);
-//    TextStyle italicBlueTitle = TextStyle(fontSize: screenWidth*0.065, fontStyle: FontStyle.italic, color: DarkCyan, fontWeight: FontWeight.bold);
-//    return new Scaffold(  
-//         appBar: AppBar(
-//           title: Text('Home Page'),
-//         ),
-//         drawer: CollapsingNavigationDrawer(),
-//         body: new Center(
-//           child: new ListView( 
-//             children: <Widget>[ 
-//               SizedBox(height: screenWidth * 0.06),
-//               Text(
-//                 'Just Clicked Cameras',
-//                 textAlign: TextAlign.center,
-//                 style: blueLargeStyle,
-//               ),
-//               SizedBox(height: screenWidth * 0.1),
-//               ListTile( 
-//                 subtitle: Text((
-//                   'Tired of scrolling on Instagram or Pinterest wishing you could be on that perfect family vacation '
-//                   +'or could take that beautiful of a picture without using a fancy filter?\n'), 
-//                   style: blueStyle),
-//               ), 
-//               ListTile( 
-//                 subtitle: Text(( 
-//                   'Well now you can!\n'
-//                   ),
-//                   style: italicBlueTitle),
-//               ), 
-//               ListTile( 
-//                 subtitle: Text((  
-//                   'Just come check out any of our Just Clicked Camera styles and we can make your wish come true! '),
-//                   style: blueStyle),
-//               ),
-//               SizedBox(height: screenWidth * 0.1),
-//               ListTile(
-//                 title: Text('About Our Company:', style: italicBlueTitle,),
-//                 subtitle: Text(
-//                   'Our three creating developers met during their Undergraduate years at South Dakota State University during '
-//                 +'the Fall of 2016. Over the years, they had many classes and projects together but in their fourth year of '
-//                 +'Undergrad they came together and thought of an idea for some product they could sell. '
-//                 +'They thought and thought for so long until the title just clicked and thus "Just Clicked Cameras" came to be.',
-//                   style: blueStyle),
-//               ),
-//               SizedBox(height: screenWidth * 0.05),
-//               ListTile(
-//                 title: Text('Special Note For Our Customers!', style: italicBlueTitle,),
-//                 subtitle: Text( 'This is only a project for a class and this does not exist in real life as a shop or a source of professional cameras, '
-//                 +'so please don\'t think this is a real company and actually use your real payment information. \nThis is just made for fun! (and class too)',
-//                   style: blueStyle),
-//               ), 
-//               SizedBox(height: screenWidth * 0.05),
-//             ],
-//           ), 
-//         ),
-//       );
-//   }
-
-// }
+}
