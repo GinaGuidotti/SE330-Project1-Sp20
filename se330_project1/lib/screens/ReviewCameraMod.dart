6decb17cc9937d2f4398942f919d3d1a0835385e
ShaneH97 <shanehill124@gmail.com>
2020-03-31 21:14:24 -0500

    creating payment pages

---
+++ se330_project1/lib/screens/ReviewCameraMod.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';  
+import 'package:se330_project1/globalvariables.dart';
+import 'package:se330_project1/navigation/custom_navigation_drawer.dart';
+
+class ReviewCameraMod extends StatefulWidget { 
+  @override
+  createState() => _ReviewCameraModPageState();
